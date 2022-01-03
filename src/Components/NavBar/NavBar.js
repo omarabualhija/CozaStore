ca6b69f81cd7e8fe687e2cda120f7512589fa560
unknown <omar.hija12>
2022-01-03 20:16:28 +0200

    COZA STORE

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -44,7 +44,7 @@ export const NavBar = (props) => {
             onClick={() => setToggleNav(!ToggleNav)}
             to="/CozaStore/Shop"
           >
-            Shope
+            Shop
           </NavLink>
           <NavLink
             onClick={() => setToggleNav(!ToggleNav)}
